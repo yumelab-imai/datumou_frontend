c3ef6110222e171eb91179d49ffbc7fdfb660c20
Masanarea <93495976+Masanarea@users.noreply.github.com>
2023-02-11 14:20:46 +0900

    [2023年度] 新規開発 | 移行作業 | 他のリポジトリからフロントエンド側の機能を移行

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    '**/*.ts',
+    '**/*.tsx',
+    '**/*.jsx',
+    '**/*.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
