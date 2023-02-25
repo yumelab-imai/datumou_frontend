2d1e2a03c7f35e376f74792b659f88d716c180cf
Masanarea <93495976+Masanarea@users.noreply.github.com>
2023-02-26 08:53:29 +0900

    [2023年度] 新規開発 | 機能実装 | クリニック登録フォーム | 二重送信防止機能 + Ajax(Axios)での非同期通信を実装

--- src/components/clinics/registerForm.tsx
+++ src/components/clinics/registerForm.tsx
@@ -2,182 +2,242 @@ import { Input, Box, Button, VStack, Select } from '@chakra-ui/react'
 // import { FormLabel } from "@chakra-ui/form-control";
 // : React.ChangeEvent<HTMLInputElement>
 import { FormControl, FormLabel, FormErrorMessage, FormHelperText, FormErrorIcon } from '@chakra-ui/form-control'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useForm } from 'react-hook-form'
+import axios from '../../libs/axios'
 
+import { GetStaticProps } from 'next'
+// axios.get('/api/prefectures')
+//       .then((response) => {
+//         let prefectures = response.data
+//         // let prefectures = JSON.parse(JSON.stringify(response.data))
+//         console.log(prefectures)
+//         // const data = res.data
+//       })
+//       .catch((error) => {
+//         // const data2 = error
+//         // console.log(data2)
+//         // return JSON.parse(JSON.stringify(data2))
+//       })
+
+// const apiSubmit = async () => {
+// return await axios
+//   .get('/api/prefectures')
+//   .then((response) => {
+//     const data = response.data
+//     // return JSON.parse(JSON.stringify(data))
+//     return data
+//   })
+//   .catch((error) => {
+//     // const data = error
+//     // return JSON.parse(JSON.stringify(data))
+//   })
+// }
+
+//   const getPrefectures = async () => {
+//     try {
+//       const response = await axios.get('/api/prefectures')
+//       // setPrefecturesArray(response.data)
+//       return response.data
+//     } catch (error) {
+//       console.error(error)
+//     }
+//   }
+//   // const prefectures1 = apiSubmit()
+//   const prefectureData = await getPrefectures()
+
+// const prefecturesArray: object[] = []
+
+// for (let i = 0; i < prefectureData.length; i++) {
+//   prefecturesArray.push(prefectureData[i])
+// }
+
+// console.log(prefecturesArray)
+// prefectures1.then((resultArray: object[]) => {
+//       for (let i = 1; i <= prefecturesArray.length; i++) {
+//         prefecturesArray.push(resultArray[i])
+//       }
+//     });
+// console.log(prefecturesArray)
+// prefecturesArray.map((value) => {
+// })
+// console.log(prefectures)
+// console.log(prefectures.map((e) => e))
+// export const Form = async ({ prefectures }: {prefectures: {}}) => {
 export const Form = () => {
+  const getPrefectures = async () => {
+    try {
+      const response = await axios.get('/api/prefectures')
+      await setPrefecturesArray(response.data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+  const [prefecturesArray, setPrefecturesArray] = useState([])
+  const [isDisabled, setIsDisabled] = useState(false)
+  // console.log(prefectures)
   const {
     handleSubmit,
     register,
-    formState: { errors, isSubmitting },
+    // formState: { isSubmitting },
+    // formState: { isSubmitted },
   } = useForm()
+  useEffect(() => {
+    getPrefectures()
+  }, [])
 
-  function onSubmit(values) {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        alert(JSON.stringify(values, null, 2))
-        resolve()
-      }, 3000)
-    })
+  async function addClinic(formData: object) {
+    return await axios
+      .post('/api/add/clinic', {
+        requestData: formData,
+      })
+      .then((response) => {
+        // console.log(response)
+        // console.log(response?.data)
+        if (response?.data?.result == true) {
+          // console.log(response)
+          // console.log(response?.data?.message)
+          alert('クリニックを登録しました。')
+        } else if (response?.data?.result == false) {
+          // console.log(response)
+          console.log(response?.data?.message)
+          console.log(response?.data?.errors)
+          alert('クリニックを登録できませんでした。')
+        } else {
+          // console.log(response)
+          alert('通信エラーが発生いたしました。もう一度お確かめください')
+        }
+        setIsDisabled(false)
+        return response?.data
+      })
+      .catch(error => {
+        console.error(error)
+        alert('エラーが発生いたしました。もう一度お確かめください')
+      })
   }
-  // const [input, setInput] = useState("");
 
-  // const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) =>
-  //     setInput(e.target.value);
+  function onSubmit(formData: object) {
+    if (confirm('本当に登録しますか?')) {
+      setIsDisabled(true)
+      // return new Promise((resolve) => {
+      //   setTimeout(() => {
+      //     alert(JSON.stringify(values, null, 2))
+      //     alert('登録しました。')
+      //     resolve()
+      //   }, 3000)
+      // })
+      // try {
+        // const response = axios.post('/api/add/clinic', {
+        //   test: values,
+        // })
+        // const response = addClinic(formData);
+        addClinic(formData);
+        // console.log(response)
+        // responseData = response.response_data
+        // console.log('onSubmit side')
+        // if(response.result == true){
+        //   console.log(response)
+        //   console.log(response?.data?.message)
+        //   alert('クリニックを登録しました。')
+        // }else if (response?.result == false) {
+        //   console.log(response)
+        //   console.log(response?.data?.message)
+        //   alert('クリニックを登録できませんでした。')
+        // } else {
+        //   console.log(response)
+        //   alert('通信エラーが発生いたしました。もう一度お確かめください')
+        // }
+        // alert(JSON.stringify(response.data, null, 2))
+      // } catch (error) {
+      //   // console.error(error)
+      //   alert('サーバーと接続ができませんでした。もう一度お試しください。')
+      // }
+    } else {
+      alert('登録しません')
+    }
+    // return alert(JSON.stringify(values, null, 2))
+  }
+  // useEffect(() => {
+  //   // axios.get('/api/books').then((res) => {
+  //   //   // const data = res.data
+  //   //   // console.log(data)
+  //   //   const data = res.data
+  //   //   console.log(data)
+  //   // })
 
-  // const isError = input === "";
+  // }, [prefectures])
+
+
+  const test: boolean | undefined = true
 
   return (
+    //   <form onSubmit={handleSubmit(onSubmit)}>
+    //   // バリデーションエラーの内容に応じてエラーメッセージが表示される
+    //   <input {...register("userid", rules)} />
+    //   {errors.userid && errors.userid.message}
+    // </form>
+
     <form onSubmit={handleSubmit(onSubmit)}>
       <VStack>
         {/* クリニックの種類 */}
-        <FormControl isInvalid={errors.clinicCode}>
-          <FormLabel htmlFor="clinicCode">クリニックの種類</FormLabel>
-          <Select
-            id="clinicCd"
-            {...register('clinicCode', {
-              required: '必須項目です。',
-            })}
-          >
+        {/* <FormControl isInvalid={errors.category_type}> */}
+        <FormControl>
+          <FormLabel htmlFor="category_type">クリニックの種類</FormLabel>
+          <Select id="category_type" {...register('category_type')}>
             <option>--</option>
-            <option value="1">メンズエナル</option>
+            <option value="1" selected>メンズエナル</option>
             <option value="2">あおばクリニック</option>
             <option value="3">Dr.COBA</option>
           </Select>
-          <FormErrorMessage>{errors.clinicCode && errors.clinicCode.message}</FormErrorMessage>
+          {/* <FormErrorMessage>{errors.category_type && errors.category_type.message}</FormErrorMessage> */}
+          {/* <FormErrorMessage>error</FormErrorMessage> */}
         </FormControl>
         {/* 店舗名 */}
-        <FormControl isInvalid={errors.storeName}>
-          <FormLabel htmlFor="storeName">店舗名</FormLabel>
-          <Input
-            id="storeName"
-            placeholder="クリニック名"
-            {...register('storeName', {
-              required: '必須項目です。',
-              minLength: {
-                value: 2,
-                message: '2文字以上で登録してください。',
-              },
-            })}
-          />
-          <FormErrorMessage>{errors.storeName && errors.storeName.message}</FormErrorMessage>
+        <FormControl>
+          <FormLabel htmlFor="clinic_name">店舗名</FormLabel>
+          <Input id="clinic_name" placeholder="クリニック名" {...register('clinic_name')} />
+          {/* <FormErrorMessage>error</FormErrorMessage> */}
         </FormControl>
         {/* 都道府県 */}
-        <FormControl isInvalid={errors.clinicPrefecture}>
-          <FormLabel htmlFor="clinicPrefectureCode">都道府県</FormLabel>
-          <Select
-            id="clinicPrefecture"
-            {...register('clinicPrefecture', {
-              required: '必須項目です。',
-            })}
-          >
+        <FormControl>
+          <FormLabel htmlFor="prefecture_cd">都道府県</FormLabel>
+          <Select id="prefecture_cd" {...register('prefecture_cd')}>
             {/* map Version */}
-            {/* {gender.map((item, i) => (
-                            <option value={item.key} key={item.key}>
-                                {item.label}
-                            </option>
-                        ))} */}
             <option>--</option>
-            <option value="1">北海道</option>
-            <option value="2">青森県</option>
-            <option value="3">岩手県</option>
-            <option value="4">宮城県</option>
-            <option value="5">秋田県</option>
-            <option value="6">山形県</option>
-            <option value="7">福島県</option>
-            <option value="8">茨城県</option>
-            <option value="9">栃木県</option>
-            <option value="10">群馬県</option>
-            <option value="11">埼玉県</option>
-            <option value="12">千葉県</option>
-            <option value="13">東京都</option>
-            <option value="14">神奈川県</option>
-            <option value="15">新潟県</option>
-            <option value="16">富山県</option>
-            <option value="17">石川県</option>
-            <option value="18">福井県</option>
-            <option value="19">山梨県</option>
-            <option value="20">長野県</option>
-            <option value="21">岐阜県</option>
-            <option value="22">静岡県</option>
-            <option value="23">愛知県</option>
-            <option value="24">三重県</option>
-            <option value="25">滋賀県</option>
-            <option value="26">京都府</option>
-            <option value="27">大阪府</option>
-            <option value="28">兵庫県</option>
-            <option value="29">奈良県</option>
-            <option value="30">和歌山県</option>
-            <option value="31">鳥取県</option>
-            <option value="32">島根県</option>
-            <option value="33">岡山県</option>
-            <option value="34">広島県</option>
-            <option value="35">山口県</option>
-            <option value="36">徳島県</option>
-            <option value="37">香川県</option>
-            <option value="38">愛媛県</option>
-            <option value="39">高知県</option>
-            <option value="40">福岡県</option>
-            <option value="41">佐賀県</option>
-            <option value="42">長崎県</option>
-            <option value="43">熊本県</option>
-            <option value="44">大分県</option>
-            <option value="45">宮崎県</option>
-            <option value="46">鹿児島県</option>
-            <option value="47">沖縄県</option>
+            {prefecturesArray.map((item, i) => (
+              <option value={item.category_key} key={item.id}>
+                {item.category_value}
+              </option>
+            ))}
           </Select>
-          <FormErrorMessage>{errors.clinicPrefecture && errors.clinicPrefecture.message}</FormErrorMessage>
+          {/* <FormErrorMessage>error</FormErrorMessage> */}
         </FormControl>
         {/* クリニックの住所 */}
-        <FormControl isInvalid={errors.clinicAddress}>
-          <FormLabel htmlFor="clinicAddress">クリニックの住所</FormLabel>
-          <Input
-            id="clinicAddress"
-            placeholder="クリニックの住所"
-            {...register('clinicAddress', {
-              required: '必須項目です。',
-              minLength: {
-                value: 8,
-                message: '8文字以上で登録してください。',
-              },
-            })}
-          />
-          <FormErrorMessage>{errors.clinicAddress && errors.clinicAddress.message}</FormErrorMessage>
+        {/* <FormControl isInvalid={errors.clinic_address}> */}
+        <FormControl>
+          <FormLabel htmlFor="clinic_address">クリニックの住所</FormLabel>
+          <Input id="clinic_address" placeholder="クリニックの住所" {...register('clinic_address')} />
+          {/* <FormErrorMessage>{errors.clinic_address && errors.clinic_address.message}</FormErrorMessage> */}
+          {/* <FormErrorMessage>error</FormErrorMessage> */}
         </FormControl>
         {/* 緯度 */}
-        <FormControl isInvalid={errors.latitude}>
+        {/* <FormControl isInvalid={errors.latitude}> */}
+        <FormControl>
           <FormLabel htmlFor="latitude">緯度</FormLabel>
-          <Input
-            id="latitude"
-            placeholder="緯度"
-            {...register('latitude', {
-              required: '必須項目です。',
-              minLength: {
-                value: 6,
-                message: '6文字以上で登録してください。',
-              },
-            })}
-          />
-          <FormErrorMessage>{errors.latitude && errors.latitude.message}</FormErrorMessage>
+          <Input id="latitude" placeholder="緯度" {...register('latitude')} />
+          {/* <FormErrorMessage>{errors.latitude && errors.latitude.message}</FormErrorMessage> */}
+          {/* <FormErrorMessage>error</FormErrorMessage> */}
         </FormControl>
         {/* 経度 */}
-        <FormControl isInvalid={errors.longitude}>
+        {/* <FormControl isInvalid={errors.longitude}> */}
+        <FormControl>
           <FormLabel htmlFor="longitude">経度</FormLabel>
-          <Input
-            id="longitude"
-            placeholder="経度"
-            {...register('longitude', {
-              required: '必須項目です。',
-              minLength: {
-                value: 6,
-                message: '6文字以上で登録してください。',
-              },
-            })}
-          />
-          <FormErrorMessage>{errors.longitude && errors.longitude.message}</FormErrorMessage>
+          <Input id="longitude" placeholder="経度" {...register('longitude')} />
+          {/* <FormErrorMessage>{errors.longitude && errors.longitude.message}</FormErrorMessage> */}
+          {/* <FormErrorMessage>error</FormErrorMessage> */}
         </FormControl>
         <Button
-          isLoading={isSubmitting}
+          isLoading={isDisabled}
           type="submit"
           background="blue.300"
           color="white"
@@ -189,3 +249,28 @@ export const Form = () => {
     </form>
   )
 }
+
+// export const getStaticProps: GetStaticProps = async () => {
+//   // export async function getStaticProps() でも同じ
+//   // const res = await fetch(`https://.../data`)
+//   // const posts = await res.json()
+//   // Call an external API endpoint to get posts
+//   const res = await fetch('http://localhost:8888/api/prefectures')
+//   console.log(res)
+//   const prefectures: object = await res.json()
+//   console.log(prefectures)
+
+//   if (!prefectures) {
+//     return {
+//       notFound: true,
+//     }
+//   }
+
+//   // By returning { props: { posts } }, the Blog component
+//   // will receive `posts` as a prop at build time
+//   return {
+//     props: {
+//       prefectures,
+//     },
+//   }
+// }
--- src/pages/clinics/add.tsx
+++ src/pages/clinics/add.tsx
@@ -1,7 +1,9 @@
 // import React, { useEffect } from 'react';
 
-import * as React from 'react'
-
+// import * as React from 'react'
+import React, { useState, useEffect } from 'react'
+import { GetStaticProps } from 'next'
+import axios from '../../libs/axios'
 import {
   ChakraProvider,
   Button,
@@ -14,8 +16,36 @@ import {
 } from '@chakra-ui/react'
 import { Form } from '../../components/clinics/registerForm'
 
+interface HomeProps {
+  prefectures: Record<string, any>
+}
+
+// export default function Home({ prefecturesTest }: HomeProps) {
 export default function Home() {
+  // console.log(prefectures)
   const { isOpen, onClose, onOpen } = useDisclosure()
+  // const prefectures: {} = {}
+  // useEffect(() => {
+  // axios.get('/api/books').then((res) => {
+  //   // const data = res.data
+  //   // console.log(data)
+  //   const data = res.data
+  //   console.log(data)
+  // })
+  // axios
+  //   .get('/api/prefectures')
+  //   .then((response) => {
+  //     const data1 = response.data
+  //     console.log(data1)
+  //     return JSON.parse(JSON.stringify(data1))
+  //     // const data = res.data
+  //   })
+  //   .catch((error) => {
+  //     const data2 = error
+  //     console.log(data2)
+  //     return JSON.parse(JSON.stringify(data2))
+  //   })
+  // }, [])
 
   return (
     <ChakraProvider>
@@ -25,6 +55,7 @@ export default function Home() {
         <ModalContent>
           <ModalHeader>クリニック登録フォーム</ModalHeader>
           <ModalBody>
+            {/* <Form prefecturesTest={prefecturesTest} /> */}
             <Form />
           </ModalBody>
         </ModalContent>
@@ -32,3 +63,39 @@ export default function Home() {
     </ChakraProvider>
   )
 }
+
+// export const getStaticProps: GetStaticProps = async () => {
+
+// const apiSubmit = async () => {
+//   return await axios
+//     .get('/api/books')
+//     .then((response) => {
+//       const data = response.data
+//       return JSON.parse(JSON.stringify(data))
+//     })
+//     .catch((error) => {
+//       const data = error
+//       return JSON.parse(JSON.stringify(data))
+//     })
+// }
+// axios.get('/api/books').then((res) => {
+//   // const data = res.data
+//   // console.log(data)
+//   const data = res.data
+//   console.log(data)
+// })
+// const prefecturesTest: HomeProps = await apiSubmit()
+
+// if (!prefecturesTest) {
+//   return {
+//     notFound: true,
+//   }
+// }
+
+// return {
+//   props: {
+//     prefecturesTest,
+//   },
+// }
+
+// }
